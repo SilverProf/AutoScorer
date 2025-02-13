31caae53f3f42374a939de5ca6ca0179162464b7
SilverProf <teachermankj@gmail.com>
2025-02-12 21:33:14 -0500

    Revert "Added Solution 001"

--- Scripts/main_text.gd
+++ Scripts/main_text.gd
@@ -13,12 +13,6 @@ extends Label
 ########################################################################################################
 
 
-# We'll use this variable to delay increasing the player's score
-var delay: int = 0
-
-# We'll use this variable to track the player's current score
-var score: int = 0
-
 
 ####################################################################################################
 ####																							####
@@ -29,19 +23,6 @@ var score: int = 0
 ####################################################################################################
 func _process(delta: float) -> void:
 	
-	# We'll increase the delay's value by 1, marking 1 frame of delay
-	delay += 1
-	
-	# Next, we'll check to see if we've been waiting for 60 frames (60 frames = 1 second)
-	if delay >= 60:
-		
-		# If 1 second has passed, we'll increase the player's score
-		score += 1
-		
-		# Then, we'll reset the delay
-		delay = 0
-		
-		# And finally, we'll update the label to show the player's new score.
-		# This text variable is a built-in field of the Label class.
-		# Set its value (a string) to change what the text says.
-		text = "Score: {0}".format([score])
+	# This text variable is a built-in field of the Label class.
+	# Set its value (a string) to change what the text says.
+	text = "Score: 0"
