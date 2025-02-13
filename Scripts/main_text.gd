0dee87a8b7013bd941f194a51d545addf07784d7
SilverProf <teachermankj@gmail.com>
2025-02-12 21:14:35 -0500

    Initial commit.
    Added Godot files.

---
+++ Scripts/main_text.gd
@@ -0,0 +1,29 @@
+extends Label
+
+
+
+########################################################################################################
+####																								####
+####											AUTO SCORER											####
+####																								####
+####	This is a simple challenge to make an auto-scoring idle game.								####
+####	Use the _process function make the player score every second to complete this challenge.	####
+####																								####
+####											GOOD LUCK!											####
+####																								####
+########################################################################################################
+
+
+
+####################################################################################################
+####																							####
+####	This is the _process function. It runs once every frame.								####
+####	The delta parameter is the amount of time (in seconds) that passes between frames.		####
+####	Write your code here for this project.													####
+####																							####
+####################################################################################################
+func _process(delta: float) -> void:
+	
+	# This text variable is a built-in field of the Label class.
+	# Set its value (a string) to change what the text says.
+	text = "Score: 0"
